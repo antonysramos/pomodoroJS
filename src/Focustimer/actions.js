c036bc766d50cb31bb72cdaef1929b4dd8a1b960
antony <antonysouzar@hotmail.com>
2023-12-13 12:50:48 -0300

    sixth commit actions add AULA:CONECTANDO AS PRIMEIRAS AÇÕES

---
+++ src/Focustimer/actions.js
@@ -0,0 +1,15 @@
+export function toggleRunning () {
+  console.log('toggleRunning function')
+}
+
+export function set () {
+  console.log('set function')
+}
+
+export function reset () {
+  console.log('reset function')
+}
+
+export function toggleMusic () {
+  console.log('toggleMusic function')
+}
